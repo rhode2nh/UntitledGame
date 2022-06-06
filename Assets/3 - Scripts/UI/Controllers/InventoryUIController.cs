081be19ab86ed17fb37008c9f0a5202860eb3097
Nick <rhodesn10@gmail.com>
2022-06-06 12:02:53 -0400

    Reworked file tree and cleaned up unused files

---
+++ Assets/3 - Scripts/UI/Controllers/InventoryUIController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIController : MonoBehaviour
+{
+    //private MouseLook mouseLook;
+    public GameObject inventoryUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mouseLook = GetComponent<MouseLook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    public void OpenInventory()
+    {
+        inventoryUI.SetActive(true);
+        //Time.timeScale = 0;
+        //mouseLook.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void CloseInventory()
+    {
+        inventoryUI.SetActive(false);
+        //Time.timeScale = 1;
+        //mouseLook.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
